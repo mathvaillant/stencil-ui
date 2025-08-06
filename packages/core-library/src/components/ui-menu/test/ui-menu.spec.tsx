22344b299dea5c44c24fc4b460229c9232289c87
mathvaillant <matheusvaillant505@gmail.com>
2025-08-06 18:28:57 +0100

    Unit Testing

--- packages/core-library/src/components/ui-menu/test/ui-menu.spec.tsx
+++ packages/core-library/src/components/ui-menu/test/ui-menu.spec.tsx
@@ -1,18 +1,103 @@
+import { h } from '@stencil/core';
 import { newSpecPage } from '@stencil/core/testing';
 import { UIMenu } from '../ui-menu';
+import { UIMenuItem } from '../ui-menu.types';
 
-describe('ui-menu', () => {
-  it('renders', async () => {
-    const page = await newSpecPage({
+describe('Menu Component', () => {
+  async function renderMenu(items?: UIMenuItem[]) {
+    return newSpecPage({
       components: [UIMenu],
-      html: `<ui-menu></ui-menu>`,
+      template: () => (
+        <ui-menu
+          open={false}
+          data={
+            items || [
+              {
+                id: 'item1',
+                label: 'Item 1',
+                icon: 'notification',
+                onClick: () => console.log('Item 1 clicked'),
+              },
+              {
+                id: 'item2',
+                label: 'Item 2',
+                icon: 'settings',
+                onClick: () => console.log('Item 2 clicked'),
+              },
+              {
+                id: 'item3',
+                label: 'Item 3',
+                icon: 'delete',
+                onClick: () => console.log('Item 3 clicked'),
+              },
+              {
+                id: 'item4',
+                label: 'Item 4',
+                icon: 'edit',
+                onClick: () => console.log('Item 4 clicked'),
+              },
+              {
+                id: 'item5',
+                label: 'Item 5',
+                icon: 'menu',
+                onClick: () => console.log('Item 5 clicked'),
+              },
+            ]
+          }
+        >
+          <button id="trigger" />
+        </ui-menu>
+      ),
     });
-    expect(page.root).toEqualHtml(`
-      <ui-menu>
-        <mock:shadow-root>
-          <slot></slot>
-        </mock:shadow-root>
-      </ui-menu>
-    `);
+  }
+
+  describe('When Clicking on a trigger', () => {
+    it('should toggle the menu visibility', async () => {
+      const { root, rootInstance, waitForChanges } = await renderMenu();
+
+      const list = root.shadowRoot.querySelector('ul[role="menu"]') as HTMLUListElement;
+      expect(list.style.display).toBe('none');
+
+      const button = root.querySelector('#trigger') as HTMLButtonElement;
+      button.addEventListener('click', () => (rootInstance.open = true));
+      button.click();
+
+      await waitForChanges();
+
+      expect(list.style.display).toBe('block');
+    });
+  });
+
+  describe('When clicking on the first option', () => {
+    it('should call the onClick handler for Item 1 and close menu', async () => {
+      const onClickMock = jest.fn();
+
+      const { root, waitForChanges } = await renderMenu([
+        {
+          id: 'item1',
+          label: 'Item 1',
+          icon: 'notification',
+          onClick: onClickMock,
+        },
+      ]);
+
+      const button = root.querySelector('#trigger') as HTMLButtonElement;
+      button.click();
+
+      await waitForChanges();
+
+      const item = root.shadowRoot.querySelector(
+        'li[role="menuitem"] button',
+      ) as HTMLLIElement;
+
+      item.click();
+
+      await waitForChanges();
+
+      expect(onClickMock).toHaveBeenCalled();
+
+      const list = root.shadowRoot.querySelector('ul[role="menu"]') as HTMLUListElement;
+      expect(list.style.display).toBe('none');
+    });
   });
 });
--- packages/core-library/src/components/ui-tab/test/ui-tab.spec.tsx
+++ packages/core-library/src/components/ui-tab/test/ui-tab.spec.tsx
@@ -1,18 +1,81 @@
+import { h } from '@stencil/core';
 import { newSpecPage } from '@stencil/core/testing';
+import { UITabContext } from '../ui-tab-context';
+import { UITabList } from '../ui-tab-list';
 import { UITab } from '../ui-tab';
+import { UITabPanel } from '../ui-tab-panel';
 
-describe('ui-tab', () => {
-  it('renders', async () => {
-    const page = await newSpecPage({
-      components: [UITab],
-      html: `<ui-tab></ui-tab>`,
+global.MutationObserver = class {
+  constructor() {}
+  disconnect() {}
+  observe() {}
+  takeRecords(): MutationRecord[] {
+    return [];
+  }
+};
+
+describe('Tabs Component', () => {
+  async function renderTabs() {
+    return newSpecPage({
+      components: [UITabContext, UITabList, UITab, UITabPanel],
+      template: () => (
+        <ui-tab-context value="1">
+          <ui-tab-list>
+            <ui-tab value="1" label="Tab 1"></ui-tab>
+            <ui-tab value="2" label="Tab 2"></ui-tab>
+            <ui-tab value="3" label="Tab 3"></ui-tab>
+            <ui-tab value="4" label="Tab 4"></ui-tab>
+            <ui-tab value="5" label="Tab 5"></ui-tab>
+          </ui-tab-list>
+          <ui-tab-panel value="1">Content for Tab 1</ui-tab-panel>
+          <ui-tab-panel value="2">Content for Tab 2</ui-tab-panel>
+          <ui-tab-panel value="3">Content for Tab 3</ui-tab-panel>
+          <ui-tab-panel value="4">Content for Tab 4</ui-tab-panel>
+          <ui-tab-panel value="5">Content for Tab 5</ui-tab-panel>
+        </ui-tab-context>
+      ),
     });
-    expect(page.root).toEqualHtml(`
-      <ui-tab>
-        <mock:shadow-root>
-          <slot></slot>
-        </mock:shadow-root>
-      </ui-tab>
-    `);
+  }
+
+  describe('Given a value 1 to the tab context', () => {
+    it('it should show the tab and panel with value 1 as active', async () => {
+      const { root } = await renderTabs();
+
+      const activeTab = root.querySelector('button[role="tab"].active');
+      const activePanel = root.querySelector('div[role="tabpanel"]');
+
+      const activeTabId = activeTab?.getAttribute('id');
+      const activePanelId = activePanel?.getAttribute('id');
+
+      expect(activeTab).toBeDefined();
+      expect(activeTab.textContent).toBe('Tab 1');
+      expect(activeTab.getAttribute('aria-selected')).not.toBeNull();
+      expect(activeTab.getAttribute('aria-controls')).toBe(activePanelId);
+
+      expect(activePanel).toBeDefined();
+      expect(activePanel.textContent).toBe('Content for Tab 1');
+      expect(activePanel.getAttribute('aria-labelledby')).toBe(activeTabId);
+      expect(activePanel.getAttribute('aria-hidden')).toBeFalsy();
+    });
+  });
+
+  describe('When selecting a tab', () => {
+    it('it should update the active tab, panel and fire tabChange event', async () => {
+      const mockTabChange = jest.fn();
+      const { root, waitForChanges } = await renderTabs();
+
+      const tabElements = root.querySelectorAll('button[role="tab"]');
+      const [tab1, tab2] = Array.from(tabElements) as HTMLButtonElement[];
+
+      root.addEventListener('tabChange', mockTabChange);
+
+      expect(root.getAttribute('value')).toBe('1');
+      expect(tab1).toHaveClass('active');
+
+      tab2.click();
+      await waitForChanges();
+      expect(mockTabChange).toHaveBeenCalledTimes(1);
+      expect(root.getAttribute('value')).toBe('2');
+    });
   });
 });
